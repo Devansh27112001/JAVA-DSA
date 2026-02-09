fffa00af2429574d7f7bc8b2322d22f3476df7db
Devansh27112001 <dkansara2711@gmail.com>
2026-02-09 10:30:06 -0500

    --solution-optimal

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -32,8 +32,27 @@ public class ProductExceptSelf {
         }
         return result;
     }
+
+    static int[] solution_best(int[] nums){
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = 1;
+        right[nums.length - 1] = 1;
+        for(int i = 1; i < nums.length; i++) {
+            left[i] = nums[i-1] * left[i-1];
+        }
+        for(int i = nums.length - 2; i >= 0; i--) {
+            right[i] = nums[i+1] * right[i+1];
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = left[i] * right[i];
+        }
+        return nums;
+    }
     static void main() {
-        System.out.println(Arrays.toString(solutionBrute(new int[]{1,2,3,4})));
-        System.out.println(Arrays.toString(solution_better(new int[]{1,2,3,4})));
+//        System.out.println(Arrays.toString(solutionBrute(new int[]{1,2,3,4})));
+//        System.out.println(Arrays.toString(solution_better(new int[]{1,2,3,4})));
+        System.out.println(Arrays.toString(solution_best(new int[]{1,2,3,4})));
     }
 }
